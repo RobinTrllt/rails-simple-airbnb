c912c16dac0cd425b7bb1fd385c42b7557fe79ee
robin trouillet <robintrouillet@hotmail.fr>
2021-11-10 17:28:22 +0100

    rbnb css ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,45 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Flat.destroy_all
+
+puts 'Creating flats...'
+flatone = {
+  name: 'Chez Nico',
+  address: '7 Boundary St, London',
+  description: '0647409298',
+  price_per_night: 270,
+  number_of_guests: 3
+}
+
+flattwo = {
+  name: 'Chez Benj',
+  address: '20 Av Montaigne, Paris',
+  description: '0645309870',
+  price_per_night: 205,
+  number_of_guests: 2
+}
+
+flatthree = {
+  name: 'Chez Louise',
+  address: '42 rue du fion, Sainté',
+  description: '0677653098',
+  price_per_night: 55,
+  number_of_guests: 12
+}
+
+flatfour = {
+  name: 'Chez Robin',
+  address: '10 Bvd la Victoire, Lyon',
+  description: '0700865598',
+  price_per_night: 105,
+  number_of_guests: 6
+}
+
+[flatone, flattwo, flatthree, flatfour].each do |attributes|
+  flat = Flat.create!(attributes)
+  puts "Created #{flat.name}"
+end
+puts 'Finished!'
